a700c3aec764da9838ee0477122462a45ce0fe29
mudox <imudox@gmail.com>
2021-10-22 01:59:23 +0800

    remove rvm.zsh

--- zsh/init/rvm.zsh
+++
@@ -1,9 +0,0 @@
-# vim: filetype=zsh
-
-# NOTE:need source it in both login rc files (e.g. .profile) and interactive shell file (e.g. .zshrc)
-
-# Add RVM to PATH for scripting
-path+=("$HOME/.rvm/bin")
-
-# Load RVM into a shell session *as a function* in interactive shell.
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
