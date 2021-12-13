d4afd283a56f3043a7a0a3487b44f0378f23d273
mudox <imudox@gmail.com>
2021-12-13 11:02:27 +0800

    tmux.zsh: fix zinit snippet issue and enable auto start feature

--- zsh/init/tmux.zsh
+++ zsh/init/tmux.zsh
@@ -1,3 +1,10 @@
 bindkey -r '^L'
 bindkey -r '^[^L'
 bindkey '^[l' clear-screen
+
+# auto start tmux when open a new terminal app window
+export ZSH_TMUX_AUTOSTART=true
+
+# fix: zinit snippet does not download dependence files (tmux.extra.conf)
+# `default-terminal` is already set in my .tmux.conf
+export ZSH_TMUX_FIXTERM=false
