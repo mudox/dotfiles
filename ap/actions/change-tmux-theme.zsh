101c180621aabf07b5c9347a9a2464efbb83c7eb
mudox <imudox@gmail.com>
2022-01-25 01:28:40 +0800

    ap(change-tmux-theme): fix misuse of `set -e`

--- ap/actions/change-tmux-theme.zsh
+++ ap/actions/change-tmux-theme.zsh
@@ -1,16 +1,19 @@
 #!/usr/bin/env zsh
 set -euo pipefail
 
-dir=~/.tmux/tmuxline-files
+dir=~/.tmux/themes
 
-# sav old theme name
-default=badwolf
-old=${$(tmux show -gv @mdx-tmux-theme):-${default}}
+# save old theme name
+default_theme=badwolf
+old=${$(tmux show -gv @mdx-tmux-theme):-${default_theme}}
+
+# restore on error or cancellation
+trap 'echo cancelled' SIGINT
 
 # select
-file=$(\ls -1 "${dir}" | fzf --preview="tmux source $dir/{}" --preview-window=0)
+set +e
+file=$(\ls -1 "${dir}" | fzf --preview="tmux source ${dir}/{}" --preview-window=0)
 file=${file:-$old}
 
 # change (or restore)
-ret=$(tmux source "${dir}/${file}")
-
+tmux source "${dir}/${file}"
