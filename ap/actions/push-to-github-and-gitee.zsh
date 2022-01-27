82b8f81242098e2a10733d469769807de3018bdf
mudox <imudox@gmail.com>
2022-01-27 20:22:05 +0800

    ap: improve pushing action

--- ap/actions/push-to-github-and-gitee.zsh
+++ ap/actions/push-to-github-and-gitee.zsh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 set -euo pipefail
 
-jack info 'Pushing to [github] & [gitee] ...'
-git push github & git push gitee & wait
+git push github &
+git push gitee &
+wait
 
-jack info 'Open in browser ...'
-open "$(git config remote.github.url)"
-open "$(git config remote.gitee.url)"
+print -n -- 'Open repo pages? '
+if read -t 2 -q; then
+  open "$(git config remote.github.url)" &
+  open "$(git config remote.gitee.url)" &
+fi
