de8195fdbb78f01d4f824c5458d1d223cf3ad74e
mudox <imudox@gmail.com>
2022-01-27 20:20:16 +0800

    tmux: improve alias `tksv`

--- zsh/init/tmux.zsh
+++ zsh/init/tmux.zsh
@@ -1,12 +1,18 @@
 # Envs
 export TMUX_AUTOQUIT=true
 
+tksv() {
+  if [[ -n $TMUX ]]; then
+    tmux detach-client
+  fi
+  tmux kill-server
+}
+
 # Aliases
 alias ta='tmux attach -t'
 alias tad='tmux attach -d -t'
 alias ts='tmux new-session -s'
 alias tl='tmux list-sessions'
-alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
 
 # Key bindings
