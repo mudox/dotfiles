3a9cd6c7060bed72bbb0b4ca324c6eac4f749219
mudox <imudox@gmail.com>
2022-03-18 03:06:48 +0800

    remove tmux auto start logic

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,17 +1,5 @@
 #  vim: fdm=marker fmr=〈,〉
 
-# NOTE: already let kitty to launch /Users/mudox/.bin/tmux immediately in `kitty.conf`
-# tmux
-# if [[ -n $KITTY_WINDOW_ID ]]; then # Only auto start Tmux in Kitty terminal
-#   if [[ -z "$TMUX" && -z "$INSIDE_EMACS" && -z "$EMACS" && -z "$VIM" ]]; then
-#     if [[ $TMUX_AUTO_STARTED != true ]]; then
-#       export TMUX_AUTO_STARTED=true
-# 			export PATH="${HOME}/.bin:${PATH}"
-#       exec tmux
-#     fi
-#   fi
-# fi
-
 # configs
 () {
   local dir=${MDX_DOT_DIR}/zsh
