9222060235f291a1b2c39100bd87bbfbc09df758
Mudox <imudox@gmail.com>
2022-05-10 21:06:53 +0800

    improve ap action - push-to-github-and-gitee

--- ap/actions/push-to-github-and-gitee.zsh
+++ ap/actions/push-to-github-and-gitee.zsh
@@ -4,9 +4,3 @@ set -euo pipefail
 git push github &
 git push gitee &
 wait
-
-print -n -- 'Open repo pages? '
-if read -t 2 -q; then
-  open "$(git config remote.github.url)" &
-  open "$(git config remote.gitee.url)" &
-fi
