1ac48ff8b7452b8f898b570284d6b15484cf3326
Mudox <imudox@gmail.com>
2022-09-24 04:01:58 +0800

    python: remove arm64 fix

--- zsh/init/90.python.zsh
+++ zsh/init/90.python.zsh
@@ -1,4 +1,4 @@
-# vim: fdm=marker fmr=〈,〉
+#  vim: fdm=marker fmr=\ 〈,\ 〉
 
 # pyenv 〈
 
@@ -8,14 +8,7 @@ if ! command -v pyenv >/dev/null 2>&1; then
 fi
 
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-
 eval "$(pyenv init -)"
-
-# issue: Invalid configuration `arm64-apple-darwin20.6.0': machine `arm64-apple' not recognized
-# see: https://github.com/pyenv/pyenv/issues/1768#issuecomment-871602950
-export PYTHON_CONFIGURE_OPTS="--build=aarch64-apple-darwin20.5.0"
 
 # 〉
 
